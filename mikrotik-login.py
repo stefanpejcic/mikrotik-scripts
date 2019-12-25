b704a7a76d530058f1bf5afb4cf08fd6c90caca1
Stefan Pejcic <pejcha1994@gmail.com>
2019-12-25 16:03:25 +0100

    Update mikrotik-login.py

--- mikrotik-login.py
+++ mikrotik-login.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-
-import re
-import requests
+import re, requests
 from md5 import md5
 
 USER = 'admin'
-PASS = 'password'
+PASS = '' #default is blank for new routers
 IP = '192.168.1.1'
 
 browser = requests.Session()
 browser.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0"}
+#browser.proxies = {"http": "http://127.0.0.1:8080","https": "http://127.0.0.1:8080"} #uncomment to use proxy
 
 #get md5 salt
 res = browser.get('http://' + IP + '/login')
@@ -17,8 +15,9 @@ regex = re.search('hexMD5\(\'(.+?)\' \+ document.login.password.value \+ \'(.+?)
 
 #if found salt, do login
 if regex :
-  hex_hash_password = md5( regex.group(1).decode('string_escape') + PASS + regex.group(2).decode('string_escape') ).hexdigest()
-  print 'hexMD5(' + regex.group(1) + PASS + regex.group(2) + ') => ' + hex_hash_password
   payload = {'username':USER, 'password':hex_hash_password, 'dst':'', 'popup':'true'}
-  res = browser.post('http://' + IP + '/login', data=payload)
+  res = browser.post('http://' + IP + '/login', data=payload) #send data to /login
+  hex_hash_password = md5( regex.group(1).decode('string_escape') + PASS + regex.group(2).decode('string_escape') ).hexdigest()
+  print ('hexMD5' + regex.group(1) + PASS + regex.group(2) + ') => ' + hex_hash_password
   # do something
+  #print res.text
